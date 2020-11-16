bf398261a56b5b70362c1ee473469cb5e270555c
kbika <kbikaco1@outlook.com>
2020-11-16 12:55:08 +0000

    Course work from week 6 (editted) rock, paper,scissors 4.16: Stretch Tasks(week4)

--- Week6Sem1/week6project/rock_paper_scissors.py
+++ Week6Sem1/week6project/rock_paper_scissors.py
@@ -10,31 +10,31 @@ count = 0
 
 while play_again == "y":
     internal_player_choice = random.choice(internal_player)
-    print(internal_player_choice)
-    user_input = input("Please make a choice; rock, paper, scissors")
+    #print(internal_player_choice)
+    user_input = input("Please make a choice; rock, paper, scissors: ")
     if user_input == internal_player_choice:
         print("It's a tie")
         tie_score += 1
-        print(internal_player_choice)
+        #print(internal_player_choice)
     elif user_input == "rock" and internal_player_choice == "scissors":
         winning_score += 1
         print("you won")
-        print(internal_player_choice)
+        #print(internal_player_choice)
     elif user_input == "scissors" and internal_player_choice == "paper":
         winning_score += 1
         print("you won")
-        print(internal_player_choice)
+        #print(internal_player_choice)
     elif user_input == "paper" and internal_player_choice == "rock":
         winning_score += 1
         print("you won")
-        print(internal_player_choice)
+        #print(internal_player_choice)
     else:
         loosing_score += 1
         print("you lost")
         print(user_input)
-        print(internal_player_choice)
+        #print(internal_player_choice)
     count += 1
-    play_again = input("Would you like to play again? Enter y = Yes, or n = No")
+    play_again = input("Would you like to play again? Enter y = Yes, or n = No: ")
 print("you won", winning_score, "out of", count)
 print("you lost", loosing_score, "out of", count)
 print(tie_score, "was tied")
